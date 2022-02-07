dd01313b842bb20435d4c53c502ab5b817603630
valer-sky <89993618+valer-sky@users.noreply.github.com>
2022-02-07 12:05:39 +0300

    add new work

--- Work/app/js/app.js
+++
@@ -1,9 +0,0 @@
-// import $ from 'jquery'; window.jQuery = $; window.$ = $ // import jQuery module (npm i -D jquery)
-
-// require('~/app/libs/mmenu/js/jquery.mmenu.all.min.js') // import vendor jQuery plugin example (not module)
-
-document.addEventListener('DOMContentLoaded', () => {
-
-	// Custom JS
-
-});
